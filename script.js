95edd265aa274cb77c214ddb8b73fe3a4293ca0d
arynttai <akosya.akmaral@gmail.com>
2024-05-05 17:20:01 +0500

    third commit

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ function openCard() {
     var card = document.querySelector('.card');
     card.classList.toggle('opened');
   }
-  
+function toggleCard() {
+  var card = document.querySelector('.card');
+  card.classList.toggle('opened');
+}
+
+document.querySelector('.click-indicator').addEventListener('click', toggleCard);
