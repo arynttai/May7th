0ae2b5c3c8cda43ca85236ebd52e627fa07b2114
arynttai <akosya.akmaral@gmail.com>
2024-05-05 17:37:28 +0500

    updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function toggleCard() {
     var card = document.querySelector('.card');
     card.classList.toggle('opened');
-  }
-  
-  document.querySelector('.click-indicator').addEventListener('click', toggleCard);
-  
+}
+
+document.querySelector('.card').addEventListener('click', function(event) {
+    toggleCard();
+    event.stopPropagation(); 
+});
+
+document.body.addEventListener('click', function(event) {
+    var card = document.querySelector('.card');
+    if (!card.contains(event.target) && card.classList.contains('opened')) {
+        toggleCard();
+    }
+});
